128093b4b09ae60e85908ef141a51e7dad0e4b75
srzzuares <srzzuares@gmail.com>
2021-12-14 22:52:06 -0600

    ProyectoOne

---
+++ src/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: '200527_DB'
+    }
+
+};
---
+++ src/routes/authentication.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+
+const passport = require('passport');
+const { isLoggedIn } = require('../lib/auth');
+
+
+router.get('/signup', (req, res) => {
+  res.render('auth/signup');
+});
+
+router.post('/signup', passport.authenticate('local.signup', {
+  successRedirect: '/profile',
+  failureRedirect: '/signup',
+  failureFlash: true
+}));
+
+
+router.get('/signin', (req, res) => {
+  res.render('auth/signin');
+});
+
+router.post('/signin', (req, res, next) => {
+  req.check('username', 'Agrege Datos').notEmpty();
+  req.check('password', 'Agrega Contraseña').notEmpty();
+  const errors = req.validationErrors();
+  if (errors.length > 0) {
+    req.flash('message', errors[0].msg);
+    res.redirect('/signin');
+  }
+  passport.authenticate('local.signin', {
+    successRedirect: '/profile',
+    failureRedirect: '/signin',
+    failureFlash: true
+  })(req, res, next);
+});
+
+router.get('/logout', (req, res) => {
+  req.logOut();
+  res.redirect('/');
+});
+
+router.get('/profile', isLoggedIn, (req, res) => {
+  res.render('profile');
+});
+
+module.exports = router;
